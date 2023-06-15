3b88e41fccdc7810dca3acef9d2cca8ca5823ab8
Mikerophonic <micgcon@gmail.com>
2023-06-15 12:39:08 -0700

    write scripts to read user input and show result

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+    const form = document.querySelector("form");
+    form.onsubmit = function(event) {
+      event.preventDefault();
+
+      const question1 = parseInt(document.querySelector("input[name='question1']:checked").value);
+      const question2 = parseInt(document.querySelector("input[name='question2']:checked").value);
+      const question3 = parseInt(document.querySelector("input[name='question3']:checked").value);
+      const question4 = parseInt(document.querySelector("input[name='question4']:checked").value);
+      const question5 = parseInt(document.querySelector("input[name='question5']:checked").value);
+      const result = question1 + question2 + question3 + question4 + question5;
+
+      const javascript = document.getElementById("javascript");
+      const cSharp = document.getElementById("c#");
+      const python = document.getElementById("python");
+      const error = document.getElementById("error");
+
+      if (result >= 24) {
+            python.removeAttribute("class");
+      } else if (result >= 17) {
+            javascript.removeAttribute("class");
+      } else if (result >= 10) {
+            cSharp.removeAttribute("class");
+      }  else {
+            error.removeAttribute("class");
+        }
+    }
+}
+
+
+
+//points: 
+/* C# - 10 to 16 
+JS - 17 to 23
+Python - 24 to 30*/
